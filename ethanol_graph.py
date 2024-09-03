18d9e0cba129eb427d5400db10f0f93496edb223
Radek Vasicek <radekvr@gmail.com>
2024-09-03 11:05:21 +0200

    graficas de los datos y los ajustes

--- ethanol_graph.py
+++ ethanol_graph.py
@@ -68,6 +68,7 @@ def plot_integral(df):
     
     # Number of groups
     num_groups = total_columns // 4
+    logger.info(f"plot_integral: number of plots = {num_groups}")
     
     # Determine number of rows needed for 2 columns
     n_cols = 3
@@ -93,7 +94,7 @@ def plot_integral(df):
         ax.plot(df[x_col], df[c21_col], label=f'C2')
         ax.plot(df[x_col], df[c31_col], label=f'C3')
     
-        ax.set_title(f'{x_col}')
+        ax.set_title(f'{x_col.strip("_x")}')
         ax.set_ylabel('Values')
         #ax.set_title(f'Plots for Group {i+1}')
         ax.legend()
@@ -103,14 +104,90 @@ def plot_integral(df):
 
     # Adjust layout
     plt.tight_layout()
-    plt.show()    
+    #plt.show()    
+
+def T1_calculation (x, y): 
+    f = lambda t,a,b : a* ( 1 -   2 * np.exp(-t / b ))
+    # Fit the function a * np.exp(b * t) + c to x and y
+    popt, pcov = curve_fit(f , x, y)
+    logger.debug(f"popt output = {popt}")
+    logger.debug(f"pcov output = {pcov}")
+    
+    return (popt[1], np.sqrt(np.diag(pcov))[1])
+
+def ethanol_t1_calc(df):
+    # Total number of columns
+    total_columns = len(df.columns)
+    
+    # Number of groups
+    num_groups = total_columns // 4
+
+    # dict list for df
+    idx_name =[]
+    OH_T1 = []
+    OH_T1_error = []
+    C2_T1 = []
+    C2_T1_error  = []
+    C3_T1 = []
+    C3_T1_error = []
+
+    # Loop through each group and plot the data
+    for i in range(num_groups):
+        # Calculate the column indices for this group
+        base_idx = i * 4
+        x_col = df.columns[base_idx]
+        oh_col = df.columns[base_idx + 1]
+        c2_col = df.columns[base_idx + 2]
+        c3_col = df.columns[base_idx + 3]
+             
+        #T1 and error calculations
+        oh_t1 , oh_t1_error = T1_calculation(df[x_col], df[oh_col])
+        c2_t1 , c2_t1_error  =  T1_calculation(df[x_col], df[c2_col])
+        c3_t1 , c3_t1_error =  T1_calculation(df[x_col], df[c3_col])
+
+        # List append 
+        idx_name.append(x_col.strip('_x'))
+        OH_T1.append(oh_t1)
+        OH_T1_error.append(oh_t1_error)
+        C2_T1.append(c2_t1)
+        C2_T1_error.append(c2_t1_error)
+        C3_T1.append(c3_t1)
+        C3_T1_error.append(c3_t1_error)
+
+## dict creation
+    values = {"sample": idx_name,
+              "OH_T1" : OH_T1,
+              "OH_T1_error": OH_T1_error,
+              "C2_T1": C2_T1,
+              "C2_T1_error": C2_T1_error,
+              "C3_T1": C3_T1,
+              "C3_T1_error": C3_T1_error
+              }
+    percentage = lambda name :  re.search(r'(\d+(?:\.\d+)?)ETHA', name)[0].strip('ETHA')
+    t1_df = pd.DataFrame(values, index = [percentage(name) for name in idx_name])  
+    logger.info(f"T1 values :\n {t1_df}")
+    return t1_df
+
 
 if __name__ == "__main__":
 
-    df =  ethanol_file_to_df("ethanol_data.txt")
+    df =  ethanol_file_to_df("ethanol_data_5.txt")
     print(df.columns)
     print(len(df.columns)/4)
     col = df.columns
     
     plot_integral(df)
-   
+
+    t1_valeus = ethanol_t1_calc(df)
+    
+    fig = plt.figure("A", figsize=(12,5))
+    plt.errorbar(t1_valeus.index, t1_valeus["OH_T1"],yerr=t1_valeus["OH_T1_error"], fmt = "*--",  label="OH")
+    plt.errorbar(t1_valeus.index, t1_valeus["C2_T1"],yerr=t1_valeus["C2_T1_error"], fmt = "*--",  label="C2" )
+    plt.errorbar(t1_valeus.index, t1_valeus["C3_T1"],yerr=t1_valeus["C3_T1_error"], fmt = "*--",  label="C3" )
+
+    plt.grid(":")
+    plt.legend()
+    plt.xlabel("% Etha")
+    plt.ylabel("T1 [s]")
+    plt.show()
+
